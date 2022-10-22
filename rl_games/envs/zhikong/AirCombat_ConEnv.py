5da5a70c334fe44b4235c3e5b254382c20e23f4d
ZiyiLiubird <liuzy@mail.nankai.edu.cn>
2022-10-22 11:12:06 +0800

    random init

--- rl_games/envs/zhikong/AirCombat_ConEnv.py
+++ rl_games/envs/zhikong/AirCombat_ConEnv.py
@@ -169,15 +169,37 @@ class AirCombatConEnv(object):
         self.red_weapon_state = dict()
         for ego_agent_name in self.red_agents:
             self.red_weapon_state[ego_agent_name] = {}
-        # setting = np.random.randint(0, 3)
-        setting = self._episode_steps % 2
+        setting = np.random.randint(0, 3)
+        # setting = self._episode_steps % 2
         if init:
             # print(self.dict_init)
-            self.obs_dict = self.env.reset(self.dict_init[setting])
-            self.prev_obs_dict = None
+            reset_settng = self.dict_init[setting]
         else:
-            self.obs_dict = self.env.reset(self.dict_reset[setting])
-            self.prev_obs_dict = None
+            reset_settng = self.dict_reset[setting]
+
+        # random set height, lat, lon, psi
+        for red_id in self.red_agents:
+            height = random.uniform(a=13000, b=30000)
+            lat = random.uniform(a=-0.3, b=0.3)
+            lon = random.uniform(-0.14, 0.14)
+            psi = random.uniform(-90, 90)
+            reset_settng['red'][red_id]['ic/h-sl-ft'] = height
+            reset_settng['red'][red_id]['ic/lat-geod-deg'] = lat
+            reset_settng['red'][red_id]['ic/long-gc-deg'] = lon
+            reset_settng['red'][red_id]['ic/psi-true-deg'] = psi
+
+        for blue_id in self.blue_agents:
+            height = random.uniform(a=13000, b=30000)
+            lat = random.uniform(a=-0.3, b=0.3)
+            lon = random.uniform(-0.14, 0.14)
+            psi = random.uniform(-90, 90)
+            reset_settng['blue'][blue_id]['ic/h-sl-ft'] = height
+            reset_settng['blue'][blue_id]['ic/lat-geod-deg'] = lat
+            reset_settng['blue'][blue_id]['ic/long-gc-deg'] = lon
+            reset_settng['blue'][blue_id]['ic/psi-true-deg'] = psi
+
+        self.obs_dict = self.env.reset(reset_settng)
+        self.prev_obs_dict = None
         obs, obs_op = self.get_obs()
         obs_dict = {}
         obs_dict['obs'], obs_dict['obs_op'] =obs, obs_op
@@ -911,7 +933,7 @@ class AirCombatConEnv(object):
         missile_alert += np.sum(ego_alert)
 
         height, area = self.area_reward()
-        height = np.sum(height)
+        height = np.sum(height) * 2
         area = np.sum(area)
 
         for al_id, ego_agent_name in enumerate(self.red_agents):
